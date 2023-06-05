fff8b97a2a86e26ce372d63dde4831662ab516f3
wolffy <wolffy@163.com>
2023-06-05 09:29:02 +0800

    Initial commit

---
+++ src/main/scala/com/wolffy/sparkstreaming/realtime/util/PropertiesUtils.scala
@@ -0,0 +1,20 @@
+package com.wolffy.sparkstreaming.realtime.util
+
+import java.util.ResourceBundle
+
+/**
+ * 配置解析类
+ */
+object PropertiesUtils {
+
+    //直接读 properties 文件，不需要后缀
+    private val bundle: ResourceBundle = ResourceBundle.getBundle("config")
+
+    def apply(key : String ):String ={
+        bundle.getString(key)
+    }
+
+    def main(args: Array[String]): Unit = {
+        println(PropertiesUtils("kafka.broker.list"))
+    }
+}
